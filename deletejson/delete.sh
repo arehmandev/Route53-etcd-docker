3277734331d5208d5014a9d73595e9b7b0da0ea7
arehmandev <arehmandev@outlook.com>
2017-01-15 08:59:47 +0000

    Finished

--- deletejson/delete.sh
+++ deletejson/delete.sh
@@ -20,9 +20,9 @@ tldname=$1
 recordnum=$(aws route53 list-resource-record-sets --hosted-zone-id $hostedzone | jq .ResourceRecordSets[].Type | wc -l)
 clustersize=$(cat newetcdips.txt | wc -l)
 
-iterations=$(expr $(clustersize) + 6)
+#iterations=$(expr $(clustersize) + 6)
 
-for (( i = "${iterations}"; i < $recordnum; i++ )); do
+for (( i = "$clustersize}"; i < $recordnum; i++ )); do
   etcdpos=$(expr $i - 4)
   dnsname=$(aws route53 list-resource-record-sets --hosted-zone-id $hostedzone | jq .ResourceRecordSets[$i].Name | tr -d "\"" | sed -e 's/\.$//')
   etcdip=$(aws route53 list-resource-record-sets --hosted-zone-id $hostedzone | jq .ResourceRecordSets[$i].ResourceRecords[].Value | tr -d "\"")
