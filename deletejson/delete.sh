a0dc85d302667a8212d91572825100b43fb6b174
arehmandev <arehmandev@outlook.com>
2017-01-15 15:42:29 +0000

    done

--- deletejson/delete.sh
+++ deletejson/delete.sh
@@ -17,7 +17,7 @@ getip > newetcdips.txt
 hostedzone=$2
 tldname=$1
 
-recordnum=$(expr $(aws route53 list-resource-record-sets --hosted-zone-id $hostedzone | jq .ResourceRecordSets[].Type | wc -l) - 3)
+recordnum=$(expr $(aws route53 list-resource-record-sets --hosted-zone-id $hostedzone | jq .ResourceRecordSets[].Type | wc -l) - 1)
 clustersize=$(expr 1 + $(cat newetcdips.txt | wc -l))
 
 
