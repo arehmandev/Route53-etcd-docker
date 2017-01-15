428c271d225ad92ce631fcba5eb2f4c78dbe3459
arehmandev <arehmandev@outlook.com>
2017-01-15 07:54:18 +0000

    Finished

--- deletejson/delete.sh
+++ deletejson/delete.sh
@@ -16,23 +16,42 @@ getip > newetcdips.txt
 
 
 hostedzone=$2
+tldname=$1
+
 realclustersize=$(cat newetcdips.txt | wc -l)
-recordnum=$(aws route53 list-resource-record-sets --hosted-zone-id $hostedzone | jq .ResourceRecordSets[].Name | grep etcd | sed 's/[^0-9]//g' | sort -n | tail -1)
+clusterbegin=$($(expr $realclustersize + 5))
+recordnum=$(aws route53 list-resource-record-sets --hosted-zone-id Z2ZYS3N4HRA09T | jq .ResourceRecordSets[].Type | wc -l)
 
 ipoverdraft=$(expr $recordnum - $realclustersize)
 echo "ipoverdraft = $ipoverdraft"
 
-clustersize=$(expr $(cat newetcdips.txt | wc -l) + 1)
+for (( i = $clusterbegin; i < $recordnum; i++ )); do
 
-for (( i = $ipoverdraft; i < $recordnum; i++ )); do
-  cat delete.json.template > delete.json
-  etcdrec=$(expr $i + 4)
-  etcdip=$(aws route53 list-resource-record-sets --hosted-zone-id $hostedzone | jq .ResourceRecordSets[$etcdrec].ResourceRecords[].Value | tr -d "\"")
-  etcdnum=etcd$i
-  sed -i "s/etcdnum/$etcdnum/g" delete.json
-  sed -i "s/etcdip/$etcdip/g" delete.json
-  sed -i "s/tldname/$1/g" delete.json
-  aws route53 change-resource-record-sets --hosted-zone-id $2 --change-batch file://delete.json
+  dnsname=$(aws route53 list-resource-record-sets --hosted-zone-id $hostedzone | jq .ResourceRecordSets[$i].Name | tr -d "\"" | sed -e 's/\.$//')
+  if [[ $dnsname == "etcd$i.$tldname" ]]; then
+    cat delete.json.template > delete.json
+    $etcdip=$(aws route53 list-resource-record-sets --hosted-zone-id $hostedzone | jq .ResourceRecordSets[$i].ResourceRecords[].Value)
+    sed -i "s/etcdnum/etcd$i/g" delete.json
+    sed -i "s/etcdip/$etcdip/g" delete.json
+    sed -i "s/tldname/$tldname/g" delete.json
+  else
+    echo "Number $i on the list wasnt deleted"
+  fi
 done
+
+
+
+#clustersize=$(expr $(cat newetcdips.txt | wc -l) + 1)
+
+#for (( i = $ipoverdraft; i < $recordnum; i++ )); do
+#  cat delete.json.template > delete.json
+#  etcdrec=$(expr $i + 4)
+#  etcdip=$(aws route53 list-resource-record-sets --hosted-zone-id $hostedzone | jq .ResourceRecordSets[$etcdrec].ResourceRecords[].Value | tr -d "\"")
+#  etcdnum=etcd$i
+#  sed -i "s/etcdnum/$etcdnum/g" delete.json
+#  sed -i "s/etcdip/$etcdip/g" delete.json
+#  sed -i "s/tldname/$1/g" delete.json
+#  aws route53 change-resource-record-sets --hosted-zone-id $2 --change-batch file://delete.json
+#done
 # Usage: $0 tld hosted-zone-id
 # requirements: jq, awscli, curl
